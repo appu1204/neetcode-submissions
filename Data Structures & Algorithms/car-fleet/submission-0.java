2e3eb8747f0cbeebaf2c9d4efd343036c511761e
appu1204 <145522940+appu1204@users.noreply.github.com>
2026-09-08 10:44:48 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        Integer[] cars = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            cars[i] = i;
+        }
+
+        Arrays.sort(cars, (a, b) -> position[b] - position[a]);
+
+        Deque<Double> st = new ArrayDeque<>();
+
+        for (int i : cars) {
+            double time = (double) (target - position[i]) / speed[i];
+            
+            if (st.isEmpty() || time > st.peek()) {
+                st.push(time);
+            }
+        }
+
+        return st.size();
+    }
+}
